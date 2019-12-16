69f017ba7ad02a9354f33294a5418b51d2b5fdd4
Pinja Hokkanen <pinjahokkanen@Pinjas-MacBook-Pro.local>
2019-12-16 09:52:21 +0200

    Part 2 testing stuff

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
     return (
       <div>
-        <p> Hello {props.name}, you are {props.age} years old</p>
+        <p> Hello {name}, you are {age} years old</p>
+        <p>So you were probably born {bornYear()}</p>
       </div>
     )
   }
-
 
   const App = () => {
     const name = 'Pekka'
---
+++ osa1/part2/src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0)
+    const setToValue = (value) => setCounter(value)
+  
+      return (
+        <div>
+            <Display counter={counter}/>
+            <Button
+                onClick={() => setToValue(counter + 1)}
+                text='plus'
+            />
+            <Button
+                onClick={() => setToValue(counter - 1)}
+                text='minus'
+            />
+            <Button
+                onClick={() => setToValue(0)}
+                text='zero'
+            />
+        </div>
+      )
+    }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+)
+  
+  ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+  )
